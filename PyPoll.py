c926e91c12cd585bcefe18deb544c867299c1ea9
Luno0811 <rosshalley08@gmail.com>
2022-09-05 11:07:27 -0500

    added counter

--- PyPoll.py
+++ PyPoll.py
@@ -5,13 +5,21 @@ import os
 
 file_to_load = os.path.join("Resources", "election_results.csv")
 
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+total_votes = 0
+
 with open(file_to_load) as election_data:
 
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
     print(headers)
 
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+    for row in file_reader:
+        total_votes += 1
+
+
+
 
 # with open(file_to_save, "w") as txt_file:
 
