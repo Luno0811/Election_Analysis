c0a8f1a02deccdaa175e929efa181bd409c56e12
Luno0811 <rosshalley08@gmail.com>
2022-09-05 10:31:11 -0500

    added breif code line

--- PyPoll.py
+++ PyPoll.py
@@ -7,14 +7,13 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 
 with open(file_to_load) as election_data:
 
-    # Print the file object.
-     print(election_data)
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-with open(file_to_save, "w") as txt_file:
-
-    file_reader = csv.reader(election_data)
+# with open(file_to_save, "w") as txt_file:
 
 # Write down the names of all the candidates.
 # Add a vote count for each candidate.
